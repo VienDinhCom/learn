97072d453994ad3882d97e74f4373fefa6c0b223
Vien Dinh <maxvien@gmail.com>
2018-03-29 16:47:08 +0700

    express/vidjot 03-vidjot-project-start-express-fundamentals

--- express/vidjot/app/index.js
+++ express/vidjot/app/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
 
 const app = express();
 const port = 3000;
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome!';
+  res.render('index', { title });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 app.listen(3000, () => {
   console.log(`Server started on port ${port}`); // eslint-disable-line
